c6025b3f339b97e3bf3b7182dc5d3e8948f71779
akalanka <akalanka@echonlabs.com>
2023-09-23 16:09:11 +0530

    feat: weather aoo

--- script.js
+++ script.js
@@ -3,3 +3,5 @@
 // console.log(title.innerText);
 
 // title.innerText = "updated from js file"
+// 550581e0a5332452bdb4525191a7ed75
+// https://api.openweathermap.org/data/2.5/weather?q=colombo&appid=550581e0a5332452bdb4525191a7ed75&units=metric
